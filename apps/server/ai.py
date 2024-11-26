509b5e4f6e17668c33da5ac093658f6079426f5f
jimfortin <jimfortin@gmail.com>
2024-11-26 08:34:32 -0500

    Prompt ajusté pour sortie constante JSON

--- apps/server/ai.py
+++ apps/server/ai.py
@@ -5,7 +5,7 @@ from fastapi import HTTPException
 
 # Fonction pour analyser les logs
 async def analyze_logs():
-    log_file_path = os.path.join('logs', 'data2.log')  # Chemin vers le fichier logs
+    log_file_path = os.path.join('logs', 'data3.log')  # Chemin vers le fichier logs
     if not os.path.exists(log_file_path):
         raise HTTPException(status_code=404, detail="Log file not found")
 
@@ -15,9 +15,17 @@ async def analyze_logs():
         # Préparer le prompt pour Ollama
     prompt = f"""
     You are an Intrusion Detection System (IDS) that analyzes log files. 
-    Please analyze the following logs and return a JSON response indicating whether there is a problem or not. 
-    Give me the number of malicious events detected. Ignore the malicious column for anomaly detection
+    Please analyze the following logs and return a JSON response indicating whether there is a malicious events or not. 
+    In the output please identify : is_malicious_event_detected, number of malicious event detected, most frequently malicious event detected
 
+    Structure of the JSON output : 
+        is_malicious_event_detected: string,
+        number_of_malicious_events_detected: string,
+        most_frequent_malicious_event: 
+            event_type: string,
+            source_ip: ip address,
+            destination_ip: ip address,
+            message: string
     Logs:
     {logs}
     """
